30b69c34c1e09ec7de617ddd816726444e308d18
harsh gupta <guptasecularharsh@gmail.com>
2025-05-20 08:05:33 +0530

    Fix: Added missing Route import in App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Login from './Login';
+import AdminLogin from './AdminLogin';
+
 import AdminDashboard from './AdminDashboard';
 import FeedbackForm from './FeedbackForm';
 import './App.css';
@@ -13,7 +14,8 @@ function App() {
           {/* Default route shows feedback form */}
           <Route path="/" element={<FeedbackForm />} />
           {/* Admin login page */}
-          <Route path="/login" element={<Login />} />
+          <Route path="/login" element={<AdminLogin />} />
+
           {/* Admin dashboard after login */}
           <Route path="/admin" element={<AdminDashboard />} />
         </Routes>
